500025f355cad215e81f65be7522c7479d3aa34d
Helloezzi <hwjung@DESKTOP-OG312S0.localdomain>
2022-09-13 17:12:23 +0900

    update

---
+++ includes/socket.h
@@ -0,0 +1,46 @@
+#pragma	once
+
+#ifndef	__SOCKET_H__
+#define	__SOCKET_H__
+
+// Networking namespace
+namespace Morai_Net {
+	// Socket class
+	class Socket {
+		int	unixSocket;		// Unix socket handle type
+
+	public:
+		Socket();
+		template<typename T>
+		explicit Socket(T&);
+		~Socket();
+
+		bool	open();
+		bool	close();
+		int	send(const char* msg, const int size) const;
+		int	receive(char* msg, const int size) const;
+
+		template<typename T>
+		void	fromImpl(T&);
+		template<typename T>
+		T&	toImpl();
+	};
+
+	template<typename T>
+	Socket::Socket(T &soc) {
+		fromImpl<T>(soc);
+	}
+
+	template<typename T>
+	void Socket::fromImpl(T &soc) {
+		unixSocket = static_cast<int>(soc);
+	}
+
+	template<typename T>
+	T& Socket::toImpl() {
+		return static_cast<T&>(unixSocket);
+	}
+}
+#endif	// __SOCKET_H__
+
+
