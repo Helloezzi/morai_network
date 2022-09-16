2009d4a0bdb09b5429c6d556ced6b5c0c9d3fa75
Helloezzi <hwjung@DESKTOP-OG312S0.localdomain>
2022-09-16 10:09:49 +0900

    update

--- tcp_server/tcp_server.h
+++ tcp_server/tcp_server.h
@@ -10,28 +10,26 @@
 
 #include "socket.h"
 
-namespace Morai_Net {
-    class tcp_server
-    {
-    private:
-        int m_port;
-        sockaddr_in m_server;
-        Socket m_socketServer;
-        bool m_isRunnig;
-    public:
-        tcp_server();
-        tcp_server(const unsigned int port);
-        ~tcp_server();
+class tcp_server
+{
+private:
+    int m_port;
+    sockaddr_in m_server;
+    Socket m_socketServer;
+    bool m_isRunnig;
+public:
+    tcp_server();
+    tcp_server(const unsigned int port);
+    ~tcp_server();
 
-        bool start();
-        bool stop();
+    bool start();
+    bool stop();
 
-        bool send(const Socket &id, const char* msg, const int size);
-        bool receive(const Socket &id, const char* msg, const int size);
+    bool send(const Socket &id, const char* msg, const int size);
+    bool receive(const Socket &id, const char* msg, const int size);
 
-        Socket* waitForConnect();
-        bool running();
-    };
-}
+    Socket* waitForConnect();
+    bool running();
+};
 
 #endif // TCP_SERVER_H
